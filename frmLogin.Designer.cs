5518a34e10443b73d76e0b23a05908f47bf0cad1
loplop05 <ammaralharoon@hotmail.com>
2026-02-14 00:48:52 +0300

    Login Page Design & Validation

--- frmLogin.Designer.cs
+++ frmLogin.Designer.cs
@@ -3,12 +3,12 @@
     partial class frmLogin
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,86 +23,122 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            txtbxUserName = new TextBox();
+            components = new System.ComponentModel.Container();
+            LoginBtn = new Button();
             txtbxPassWord = new TextBox();
+            txtbxUserName = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
+            // 
+            // LoginBtn
+            // 
+            LoginBtn.Cursor = Cursors.Hand;
+            LoginBtn.FlatStyle = FlatStyle.Popup;
+            LoginBtn.Font = new Font("Tw Cen MT Condensed", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LoginBtn.Location = new Point(420, 453);
+            LoginBtn.Name = "LoginBtn";
+            LoginBtn.Size = new Size(251, 83);
+            LoginBtn.TabIndex = 2;
+            LoginBtn.Text = "Login";
+            LoginBtn.UseVisualStyleBackColor = true;
+            LoginBtn.Click += LoginBtn_Click;
+          
+            // 
+            // txtbxPassWord
+            // 
+            txtbxPassWord.BackColor = Color.White;
+            txtbxPassWord.Location = new Point(412, 400);
+            txtbxPassWord.Name = "txtbxPassWord";
+            txtbxPassWord.Size = new Size(259, 27);
+            txtbxPassWord.TabIndex = 1;
+            txtbxPassWord.Tag = "0000";
+           
+            // 
+            // txtbxUserName
+            // 
+            txtbxUserName.BackColor = Color.White;
+            txtbxUserName.ForeColor = SystemColors.WindowText;
+            txtbxUserName.Location = new Point(412, 261);
+            txtbxUserName.Name = "txtbxUserName";
+            txtbxUserName.Size = new Size(259, 27);
+            txtbxUserName.TabIndex = 0;
+            txtbxUserName.Tag = "admin";
+          
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Stencil", 25.8000011F, FontStyle.Bold);
+            label3.Location = new Point(412, 316);
+            label3.Name = "label3";
+            label3.Size = new Size(259, 52);
+            label3.TabIndex = 8;
+            label3.Text = "PASSWORD";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Stencil", 25.8000011F, FontStyle.Bold);
+            label2.Location = new Point(412, 186);
+            label2.Name = "label2";
+            label2.Size = new Size(259, 52);
+            label2.TabIndex = 7;
+            label2.Text = "USERNAME";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Tempus Sans ITC", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(0, 192, 0);
-            label1.Location = new Point(351, 27);
+            label1.Location = new Point(346, 22);
             label1.Name = "label1";
             label1.Size = new Size(403, 104);
-            label1.TabIndex = 0;
+            label1.TabIndex = 6;
             label1.Text = "SalaryDesk";
             // 
-            // label2
+            // errorProvider1
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Stencil", 25.8000011F, FontStyle.Bold);
-            label2.Location = new Point(440, 190);
-            label2.Name = "label2";
-            label2.Size = new Size(259, 52);
-            label2.TabIndex = 1;
-            label2.Text = "USERNAME";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Stencil", 25.8000011F, FontStyle.Bold);
-            label3.Location = new Point(440, 320);
-            label3.Name = "label3";
-            label3.Size = new Size(259, 52);
-            label3.TabIndex = 2;
-            label3.Text = "PASSWORD";
-            label3.Click += this.label3_Click;
-            // 
-            // txtbxUserName
-            // 
-            txtbxUserName.Location = new Point(440, 265);
-            txtbxUserName.Name = "txtbxUserName";
-            txtbxUserName.Size = new Size(259, 27);
-            txtbxUserName.TabIndex = 3;
-            // 
-            // txtbxPassWord
-            // 
-            txtbxPassWord.Location = new Point(440, 404);
-            txtbxPassWord.Name = "txtbxPassWord";
-            txtbxPassWord.Size = new Size(259, 27);
-            txtbxPassWord.TabIndex = 4;
+            errorProvider1.BlinkRate = 100;
+            errorProvider1.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            errorProvider1.ContainerControl = this;
             // 
             // frmLogin
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1166, 642);
+            ClientSize = new Size(1106, 640);
+            Controls.Add(LoginBtn);
             Controls.Add(txtbxPassWord);
             Controls.Add(txtbxUserName);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            IsMdiContainer = true;
             Name = "frmLogin";
-            Text = "Form1";
+            Text = "frmLogin";
+          
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private TextBox txtbxUserName;
+        private Button LoginBtn;
         private TextBox txtbxPassWord;
+        private TextBox txtbxUserName;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ErrorProvider errorProvider1;
     }
 }
--- frmLogin.cs
+++ frmLogin.cs
@@ -1,3 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
 namespace Employees_Salary_Calculation_and_Management_Application
 {
     public partial class frmLogin : Form
@@ -5,6 +13,65 @@ namespace Employees_Salary_Calculation_and_Management_Application
         public frmLogin()
         {
             InitializeComponent();
+            this.AcceptButton = LoginBtn;
         }
+
+
+
+
+
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(txtbxUserName.Text) || string.IsNullOrEmpty(txtbxPassWord.Text)))
+            {
+
+                MessageBox.Show("User Name Or Password should be Entered", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+
+            if (txtbxUserName.Text == txtbxUserName.Tag.ToString() && txtbxPassWord.Text == txtbxPassWord.Tag.ToString())
+            {
+                Form frm1 = new frmOperations();
+                frm1.ShowDialog();
+            }
+            else
+            {
+                errorProvider1.SetError(txtbxUserName, "Invalid User Name");
+                errorProvider1.SetError(txtbxPassWord, "Invalid Password");
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
